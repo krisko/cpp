9002e4b377fd8b6594a05fc8a3f8580fd5dbd01b
krisko <krissak@zoho.eu>
2019-01-09 11:13:11 +0100

    Update 00helloworld.cpp

--- 00helloworld/src/00helloworld.cpp
+++ 00helloworld/src/00helloworld.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!\nahoj2!" << endl; // prints !!!Hello World!!!
+	cout << "Hello World!\nahoj32!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
